772c846c18965ba12b46d65e2fbe8b609228df2f
Samir <sobiroff23@gmail.com>
2024-02-22 18:33:51 +0500

    fixed

--- src/companents/Header.jsx
+++ src/companents/Header.jsx
@@ -2,9 +2,12 @@ import logo from '/vite.svg'
 
 export default function Header() {
     const now = new Date()
+    
+
 
     return (
       <header>
+        <img src={logo} alt={'Results'} />
         {/* <h3>Result University</h3> */}
   
         <span>время сейчас: {now.toLocaleTimeString()}</span>
