3455bc4d20567d2621f3f71b31dd84c1ae19818c
Samir <sobiroff23@gmail.com>
2024-02-22 18:55:39 +0500

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
 import Header from "./companents/Header";
+import { ways } from "./data";
+
+function WayToTeach(props) {
+  console.log(props);
+  return (
+    <li>
+      <p>
+        <strong>{props.title}</strong>
+        {props.description}
+      </p>
+    </li>
+  );
+}
 
 export default function App() {
   return (
     <div>
       <Header />
       <main>
-        <h1>Hello React!!</h1>
+        <section>
+          <h3>Наш подхот к обучению</h3>
+
+          <ul>
+            <WayToTeach
+              title={ways[0].title}
+              description={ways[0].description}
+            />
+
+            <WayToTeach
+              title={ways[1].title}
+              description={ways[1].description}
+            />
+
+            <WayToTeach
+              title={ways[2].title}
+              description={ways[2].description}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
---
+++ src/data.js
@@ -0,0 +1,24 @@
+export const ways = [ 
+    {
+        title: 'Lorem, ipsum dolor.',
+        description:
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste expedita modi a odit hic, inventore consequuntur necessitatibus error ut distinctio voluptate animi, sequi vel consectetur impedit. Repellendus quidem itaque harum.'
+    },
+
+    {
+        title: 'MOREEEM, ipsum dolor.',
+        description:
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste expedita modi a odit hic, inventore consequuntur necessitatibus error ut distinctio voluptate animi, sequi vel consectetur impedit. Repellendus quidem itaque harum.'
+    },
+
+
+
+    {
+        title: 'AUUUU, ipsum dolor.',
+        description:
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste expedita modi a odit hic, inventore consequuntur necessitatibus error ut distinctio voluptate animi, sequi vel consectetur impedit. Repellendus quidem itaque harum.'
+    },
+
+
+    
+]
