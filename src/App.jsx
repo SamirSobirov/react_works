b4ba3246e2bbbf84a09d06aeb12f93a4a94933e7
Samir <sobiroff23@gmail.com>
2024-02-23 14:49:20 +0500

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -22,21 +22,16 @@ export default function App() {
           <h3>Наш подхот к обучению</h3>
 
           <ul>
-            
+
             <WayToTeach
               title={ways[0].title}
               description={ways[0].description}
             />
 
-            <WayToTeach
-              title={ways[1].title}
-              description={ways[1].description}
-            />
+            <WayToTeach {...ways[1]} />
+            <WayToTeach {...ways[2]} />
+            
 
-            <WayToTeach
-              title={ways[2].title}
-              description={ways[2].description}
-            />
 
           </ul>
         </section>
